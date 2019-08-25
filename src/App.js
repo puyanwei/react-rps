af0fdd13078245061be4de5b2002671d4616fb41
Puyan Wei <puyanwei@gmail.com>
2019-08-25 22:05:25 +0100

    Create react app setup, mock up of main page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className="App">
+            <div className="app-container">
+                <h1>
+                    Welcome to the Battle of<br></br> Rock Paper Scissors!
+                </h1>
+                <p>Please enter your name</p>
+                <input type="text" />
+                <button>Start</button>
+            </div>
+        </div>
+    );
+};
 
 export default App;
