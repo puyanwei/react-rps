307121e1c31986128ee8f5ba7e0e17601f7afeb2
Puyan Wei <puyanwei@gmail.com>
2019-09-08 12:23:23 +0100

    Updates

---
+++ precache-manifest.c02bb84da099611150c3ef396a3a8485.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a6a82af0a93832eba0d47a21e5663f2",
+    "url": "/react-rps/index.html"
+  },
+  {
+    "revision": "e231de85a689cd4629ca",
+    "url": "/react-rps/static/css/main.3076ad4d.chunk.css"
+  },
+  {
+    "revision": "f1a2f2a8eadc3261328b",
+    "url": "/react-rps/static/js/2.e8fcf99c.chunk.js"
+  },
+  {
+    "revision": "e231de85a689cd4629ca",
+    "url": "/react-rps/static/js/main.d9b8df31.chunk.js"
+  },
+  {
+    "revision": "31df6c3f809b06db8661",
+    "url": "/react-rps/static/js/runtime~main.095c9a8f.js"
+  }
+]);
