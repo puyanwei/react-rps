929e223d5c22495db074f78ff26ad8224463a982
Puyan Wei <puyanwei@gmail.com>
2019-09-08 11:15:43 +0100

    Mockup of outcome detection

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Result = (props) => {
+	const { playerName, playerChoice } = props.location.state;
+	const [computerChoice, setComputerChoice] = useState(null);
+
+	useEffect(() => {
+		setComputerChoice(generateComputerChoice);
+	}, []);
+
+	const getResult = () => {
+		// code that runs after the setting of the playerName and playerChoice. Will return "Win", "Lose", or "Draw"
+		const outcomes = ['WIN', 'LOSE', 'DRAW'];
+		return outcomes[Math.floor(Math.random() * outcomes.length)];
+	};
+
+	const generateComputerChoice = () => {
+		const outcomes = ['Rock', 'Paper', 'Scissors'];
+		return outcomes[Math.floor(Math.random() * outcomes.length)];
+	};
+
+	const outcome = getResult();
+
 	return (
-		<div>
-			{props.location.state.playerName} chose {props.location.state.playerChoice}
+		<div className="app-container">
+			{outcome ? (
+				<>
+					<strong>YOU {outcome}</strong>
+					<br />
+					<strong>{playerName}</strong> chose <strong>{playerChoice}</strong>
+					<br />
+					<strong>Computer</strong> chose <strong>{computerChoice}</strong>
+				</>
+			) : (
+				<>Loading...</>
+			)}
 		</div>
 	);
 };
