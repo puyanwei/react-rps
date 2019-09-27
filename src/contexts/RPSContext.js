9d4b6c0b6995c4200fa36859c1d96ac860ec5ae9
Puyan Wei <puyanwei@gmail.com>
2019-09-27 22:48:02 +0100

    Some refactoring cleaning

--- src/contexts/RPSContext.js
+++ src/contexts/RPSContext.js
@@ -7,14 +7,14 @@ const initialState = {
 	playerName: '',
 	choice: { playerChoice: null, computerChoice: null },
 	scores: { playerScore: 0, computerScore: 0 }
-}
+};
 
 const RPSContextProvider = ({ children }) => {
-	const [state, dispatch] = useReducer(RPSReducer, initialState);
+	const [ state, dispatch ] = useReducer(RPSReducer, initialState);
 
-	const contextValue = useMemo(() => ({ state, dispatch }), [state, dispatch]);
+	const contextValue = useMemo(() => ({ state, dispatch }), [ state, dispatch ]);
 
-	return <RPSContext.Provider value={contextValue}>{children}</RPSContext.Provider>
+	return <RPSContext.Provider value={contextValue}>{children}</RPSContext.Provider>;
 };
 
 export default RPSContextProvider;
--- src/reducers/RPSReducer.js
+++ src/reducers/RPSReducer.js
@@ -1,15 +1,16 @@
 export const RPSReducer = (state, action) => {
-	switch (action.type) {
+	const { type, payload } = action;
+	switch (type) {
 		case 'SET_NAME':
-			return { ...state, playerName: action.payload }
+			return { ...state, playerName: payload };
 		case 'SET_PLAYER_CHOICE':
-			return { ...state, choice: { ...state.choice, playerChoice: action.payload } }
+			return { ...state, choice: { ...state.choice, playerChoice: payload } };
 		case 'SET_COMPUTER_CHOICE':
-			return { ...state, choice: { ...state.choice, computerChoice: action.payload } }
+			return { ...state, choice: { ...state.choice, computerChoice: payload } };
 		case 'PLAYER_WINS':
-			return { ...state, scores: { ...state.scores, playerScore: state.scores.playerScore + 1 } }
+			return { ...state, scores: { ...state.scores, playerScore: state.scores.playerScore + 1 } };
 		case 'COMPUTER_WINS':
-			return { ...state, scores: { ...state.scores, computerScore: state.scores.computerScore + 1 } }
+			return { ...state, scores: { ...state.scores, computerScore: state.scores.computerScore + 1 } };
 		default:
 			throw new Error();
 	}
