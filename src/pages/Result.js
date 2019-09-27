0915a258007c931db82b758b77dd6add32d8b348
Puyan Wei <puyan.wei@mountainwarehouse.com>
2019-09-27 16:55:16 +0100

    Fix for Results.jsx. Completed useReducer, useContext refactor!

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -5,37 +5,38 @@ import './result.css';
 
 const Result = (props) => {
 	const [result, setResult] = useState(null);
+	const { state: { playerName, choice: { playerChoice, computerChoice }, scores: { playerScore, computerScore } }, dispatch } = useContext(RPSContext);
 
-	const { playerName, currentScores, setCurrentScores, choice } = useContext(RPSContext);
+	useEffect(() => {
 
-	// useEffect(() => {
-	// 	getResult(choice.player, choice.computer);
-	// }, []);
+		const getResult = (firstChoice, secondChoice) => {
+			const combinations = [
+				'Rock Scissors',
+				'Scissors Paper',
+				'Paper Rock',
+				'Rock Paper',
+				'Paper Scissors',
+				'Scissors Rock'
+			];
 
-	const getResult = (firstChoice, secondChoice) => {
-		const combinations = [
-			'Rock Scissors',
-			'Scissors Paper',
-			'Paper Rock',
-			'Rock Paper',
-			'Paper Scissors',
-			'Scissors Rock'
-		];
+			let hashIndexMatcher;
 
-		let hashIndexMatcher;
+			hashIndexMatcher = combinations.indexOf(`${firstChoice} ${secondChoice}`);
 
-		hashIndexMatcher = combinations.indexOf(`${firstChoice} ${secondChoice}`);
+			if (hashIndexMatcher === -1) {
+				setResult('DRAW');
+			} else if (hashIndexMatcher > -1 && hashIndexMatcher < 3) {
+				setResult('WIN');
+				dispatch({ type: 'PLAYER_WINS' })
+			} else {
+				setResult('LOSE');
+				dispatch({ type: 'COMPUTER_WINS' })
+			}
+		};
 
-		if (hashIndexMatcher === -1) {
-			setResult('DRAW');
-		} else if (hashIndexMatcher > -1 && hashIndexMatcher < 3) {
-			setResult('WIN');
-			setCurrentScores({ ...currentScores, player: currentScores.player + 1 });
-		} else {
-			setResult('LOSE');
-			setCurrentScores({ ...currentScores, computer: currentScores.computer + 1 });
-		}
-	};
+		getResult(playerChoice, computerChoice);
+	}, [computerChoice, dispatch, playerChoice]);
+
 
 	const handleClick = () => {
 		props.history.push({
@@ -50,15 +51,15 @@ const Result = (props) => {
 			</div>
 			<br />
 			<div>
-				<strong>{playerName}</strong> chose <strong>{choice.player}</strong>
+				<strong>{playerName}</strong> chose <strong>{playerChoice}</strong>
 			</div>
 			<br />
 			<div>
-				<strong>Computer</strong> chose <strong>{choice.computer}</strong>
+				<strong>Computer</strong> chose <strong>{computerChoice}</strong>
 			</div>
 			<div>
 				<br />
-				<strong className="large-font">{`${currentScores.player}-${currentScores.computer}`}</strong>
+				<strong className="large-font">{`${playerScore}-${computerScore}`}</strong>
 			</div>
 			<br />
 			<button onClick={handleClick}>Play again?</button>
--- src/reducers/RPSReducer.js
+++ src/reducers/RPSReducer.js
@@ -7,9 +7,9 @@ export const RPSReducer = (state, action) => {
 		case 'SET_COMPUTER_CHOICE':
 			return { ...state, choice: { ...state.choice, computerChoice: action.payload } }
 		case 'PLAYER_WINS':
-			return { ...state, scores: { ...state.scores, playerScore: state.scores.player + 1 } }
+			return { ...state, scores: { ...state.scores, playerScore: state.scores.playerScore + 1 } }
 		case 'COMPUTER_WINS':
-			return { ...state, scores: { ...state.scores, computerScore: state.scores.computer + 1 } }
+			return { ...state, scores: { ...state.scores, computerScore: state.scores.computerScore + 1 } }
 		default:
 			throw new Error();
 	}
